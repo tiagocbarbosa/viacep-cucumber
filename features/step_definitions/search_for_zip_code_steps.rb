adbf7eab8c41f27dcfe656f43ee0a437cc723cd0
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2021-04-12 18:09:13 -0300

    Logic of the step definition was created

--- features/step_definitions/search_for_zip_code_steps.rb
+++ features/step_definitions/search_for_zip_code_steps.rb
@@ -7,15 +7,32 @@ Given(/^the informed state is ([^"]*)$/) do |uf|
   unless(uf_isValid?(uf))
     raise RuntimeError, 'The informed UF is not valid'
   end
-
+  
   @url += "#{uf}/"
-  log(@url)
+end
+
+Given(/^the informed city is ([^"]*)$/) do |city|
+  unless(city_isValid?(city))
+    raise RuntimeError, 'The informed CITY is not valid'
+  end
+  
+  @url += "#{city}/"
 end
 
 #...
 
 def uf_isValid?(uf)
+  # UF must contains 2 (two) characters
   if(uf.length != 2)
+    false
+  else
+    true
+  end
+end
+
+def city_isValid?(city)
+  # city must contains at least 3 (three) characters
+  if(uf.length < 3)
     false
   else
     true
