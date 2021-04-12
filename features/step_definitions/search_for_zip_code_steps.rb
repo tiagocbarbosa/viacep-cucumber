83127dbbb971bcc1e209ed22d993122615f57e82
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2021-04-12 09:48:53 -0300

    Correction of the step definition

--- features/step_definitions/search_for_zip_code_steps.rb
+++ features/step_definitions/search_for_zip_code_steps.rb
@@ -2,14 +2,14 @@ Given('a search for a zip code') do
   @url = 'https://viacep.com.br/ws/'
 end
 
-Given(/^the informed state is "([^"]*)"$/) do |uf|
+Given(/^the informed state is ([^"]*)$/) do |uf|
   uf.upcase!
   unless(uf_isValid?(uf))
     # print error
     log('The informed UF is not valid')
   end
 
-  @url += '#{uf}'
+  @url += "#{uf}/"
   log(@url)
 end
 
